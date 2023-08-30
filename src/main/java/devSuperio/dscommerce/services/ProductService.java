b138881ec95d4d7b6c9f1f83fa9e7e445d41ee27
wlad9 <zav8b9@gmail.com>
2023-08-30 17:31:38 -0300

    Pageable

--- src/main/java/devSuperio/dscommerce/services/ProductService.java
+++ src/main/java/devSuperio/dscommerce/services/ProductService.java
@@ -4,21 +4,30 @@ import devSuperio.dscommerce.dto.ProductDTO;
 import devSuperio.dscommerce.entities.Product;
 import devSuperio.dscommerce.repositories.ProductRepository;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Repository;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import java.util.List;
 import java.util.Optional;
 
 @Service
 public class ProductService {
     @Autowired
     private ProductRepository repository;
+
     @Transactional(readOnly = true)
-    public ProductDTO findById(Long id){
+    public ProductDTO findById(Long id) {
         Optional<Product> result = repository.findById(id);
         Product product = result.get();
         ProductDTO productDTO = new ProductDTO(product);
         return productDTO;
+    }
+
+    @Transactional(readOnly = true)
+    public Page<ProductDTO> findAll(Pageable pageable) {
+        Page<Product> result = repository.findAll(pageable);
+        return result.map(x->new ProductDTO(x));
     }
 }
