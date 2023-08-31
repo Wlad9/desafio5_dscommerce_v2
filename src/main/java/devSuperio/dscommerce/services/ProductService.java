2128b4fc6ee213ecd22427668e7e1b3042108166
wlad9 <zav8b9@gmail.com>
2023-08-31 15:21:24 -0300

    DELETE

--- src/main/java/devSuperio/dscommerce/services/ProductService.java
+++ src/main/java/devSuperio/dscommerce/services/ProductService.java
@@ -53,6 +53,10 @@ public class ProductService {
         entity.setPrice(dto.getPrice());
         entity.setImgUrl(dto.getImgUrl());
     }
+    @Transactional
+    public void delete(Long id){
+        repository.deleteById(id);
+    }
 }
 
 
