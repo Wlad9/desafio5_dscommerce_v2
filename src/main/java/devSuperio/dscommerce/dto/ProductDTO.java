35ebafe7115a5027823847d38be0d2f94ead5b46
wlad9 <zav8b9@gmail.com>
2023-09-02 18:26:37 -0300

    Validação Bean no Arq: ProdutctDTO

--- src/main/java/devSuperio/dscommerce/dto/ProductDTO.java
+++ src/main/java/devSuperio/dscommerce/dto/ProductDTO.java
@@ -2,11 +2,19 @@ package devSuperio.dscommerce.dto;
 
 import devSuperio.dscommerce.entities.Product;
 import jakarta.persistence.Column;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Positive;
+import jakarta.validation.constraints.Size;
 
 public class ProductDTO {
     private Long id;
+    @Size(min = 3, max = 30, message = "Nome entre 3 e 30 caracteres")
+    @NotBlank(message = "Campo obrigatório")
     private String name;
+    @Size(min = 10, message = "Descrição com no mínimo 10 caracteres")
+    @NotBlank(message = "Campo obrigatório")
     private String description;
+    @Positive(message = "O preço deve ser positivo")
     private Double price;
     private String imgUrl;
 
